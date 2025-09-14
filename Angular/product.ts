9eecf8d9c22f8b9650e08689f2d9a2d82bc7a290
g charan <123975764+Charan-666@users.noreply.github.com>
2025-09-14 19:36:12 +0530

    Update product.ts

--- Angular/product.ts
+++ Angular/product.ts
@@ -1,11 +1,1 @@
-export interface Product{
-    productId: number;
-    productName: string;
-    price: number;
-    description: string;
-    stock: number;
-    onSale:boolean;
-    rating:number;
-   reviews:{user:string;comment:string}[]
 
-}
